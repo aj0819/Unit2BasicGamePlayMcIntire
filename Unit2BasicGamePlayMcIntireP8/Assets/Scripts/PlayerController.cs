5a21bcf523e30ff3c34b4038027f643663430c19
Adam McIntire <757770@student.dusd.net>
2024-03-25 15:29:41 -0700

    Working on challenge

--- Unit2BasicGamePlayMcIntireP8/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayMcIntireP8/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public float zRange = 10.0f;
 
     public GameObject projectilePrefab;
 
@@ -23,7 +25,6 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
@@ -31,9 +32,29 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+
+
+
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+
+
+
+
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(-zRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.z > zRange)
+        {
+            transform.position = new Vector3(zRange, transform.position.z, transform.position.z);
+        }
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
